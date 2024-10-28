6265410c8186af517d978cfba9cd2f03f4c26f76
BhuvaneshwariRajamani <brajamani@onemagnify.com>
2024-10-28 10:52:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 # Import python packages
 import streamlit as st
+from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize your smoothie :cup_with_straw:")
@@ -13,9 +15,7 @@ st.write(
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("Name on your smoothie will be: ", name_on_order)
 
-cnx = st.connection("snowflake")
-session = cnx.session()
-
+session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 ingredients_list = st.multiselect("Choose upto 5 ingredients:" , my_dataframe , max_selections = 5)
@@ -38,8 +38,3 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success("Your smoothie is ordered," + name_on_order)
-
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df = st.dataframe(data = fruityvice_response, use_container_width = true)
